1d71ecbb9402ab5434695c44fbfde4bae6e7c04b
Herohtar <belac1186@gmail.com>
2019-08-25 15:24:44 -0500

    Update DelegateRule

--- TemperatureMonitor/BaseClasses/DelegateRule.cs
+++ TemperatureMonitor/BaseClasses/DelegateRule.cs
@@ -9,25 +9,17 @@ namespace TemperatureMonitor.Utilities
     /// <typeparam name="T">The type of the object the rule can be applied to.</typeparam>
     public sealed class DelegateRule<T> : Rule<T>
     {
-        private Func<T, bool> rule;
-
-        #region Constructors
+        private readonly Func<T, bool> rule;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateRule<T>"/> class.
+        /// Initializes a new instance of the <see cref="DelegateRule&lt;T&gt;"/> class.
         /// </summary>
         /// <param name="propertyName">>The name of the property the rules applies to.</param>
         /// <param name="error">The error if the rules fails.</param>
         /// <param name="rule">The rule to execute.</param>
         public DelegateRule(string propertyName, object error, Func<T, bool> rule)
-            : base(propertyName, error)
-        {
-            this.rule = rule ?? throw new ArgumentNullException("rule");
-        }
-
-        #endregion
-
-        #region Rule<T> Members
+            : base(propertyName, error) =>
+            this.rule = rule ?? throw new ArgumentNullException(nameof(rule));
 
         /// <summary>
         /// Applies the rule to the specified object.
@@ -36,11 +28,6 @@ namespace TemperatureMonitor.Utilities
         /// <returns>
         /// <c>true</c> if the object satisfies the rule, otherwise <c>false</c>.
         /// </returns>
-        public override bool Apply(T obj)
-        {
-            return rule(obj);
-        }
-
-        #endregion
+        public override bool Apply(T obj) => rule(obj);
     }
 }
