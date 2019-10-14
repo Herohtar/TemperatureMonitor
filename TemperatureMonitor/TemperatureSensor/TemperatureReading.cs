db86f697a531f7074c83147637fdc48a87a7a2b4
Herohtar <belac1186@gmail.com>
2019-10-14 03:07:00 -0500

    Add Id (primary key) and TemperatureSensorId (foreign key) to TemperatureReading

--- TemperatureMonitor/TemperatureSensor/TemperatureReading.cs
+++ TemperatureMonitor/TemperatureSensor/TemperatureReading.cs
@@ -5,13 +5,21 @@ namespace TemperatureMonitor
 {
     public class TemperatureReading : NotifyPropertyChanges
     {
+        private int id;
         private DateTime time;
         private double temperature;
+        private string temperatureSensorId = null!;
 
         public TemperatureReading(DateTime time, double temperature)
         {
             this.time = time;
             this.temperature = temperature;
+        }
+
+        public int Id
+        {
+            get => id;
+            set => SetProperty(ref id, value);
         }
 
         public DateTime Time
@@ -24,6 +32,12 @@ namespace TemperatureMonitor
         {
             get => temperature;
             set => SetProperty(ref temperature, value);
+        }
+
+        public string TemperatureSensorId
+        {
+            get => temperatureSensorId;
+            set => SetProperty(ref temperatureSensorId, value);
         }
     }
 }
